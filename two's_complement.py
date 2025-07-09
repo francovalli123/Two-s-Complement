2b654d100800f77372c1383549a996e84340b381
Franco Valli <frankovalli123@gmail.com>
2025-07-09 11:22:09 -0300

    Bugs Fixed

--- two's_complement.py
+++ two's_complement.py
@@ -1,27 +1,32 @@
-decimalNumber = int(input("Number: ")) # Enter a number in decimal system
-decimalToBinary= bin(decimalNumber) 
-binaryNumber = decimalToBinary[2:]
+decimalNumber = int(input("Number: "))  # Ingresa un número decimal
 
-binaryNumber_toString = str(binaryNumber)
-lenght = len(binaryNumber_toString)
-array = [""] * lenght
+# Si es positivo, simplemente mostramos el binario sin el '0b' prefix
+if decimalNumber >= 0:
+    binary = bin(decimalNumber)[2:]
+    print(binary)
 
-for x in range(0,lenght):
-    array[x] = int(binaryNumber_toString[x])
+else:
+    # Si es negativo, obtenemos el complemento a dos del valor absoluto
+    abs_value = abs(decimalNumber)
+    binary = bin(abs_value)[2:]  # binario del valor absoluto
+    length = len(binary)
 
-array = array[::-1]
+    # Aseguramos un tamaño fijo (por ejemplo, 8 bits)
+    fixed_length = max(8, length)
+    binary = binary.zfill(fixed_length)
 
-for i in range(0, lenght):
-    if array[i] == 1:
-        j = i + 1
-        if array[j] == 0:
-            array[j] = 1
-        else:
-            array[j] = 0
+    # Invertimos los bits (complemento a uno)
+    inverted = ''.join('1' if bit == '0' else '0' for bit in binary)
 
-twos_complement = ""
+    # Sumamos 1 para obtener el complemento a dos
+    inverted_list = list(inverted)
+    carry = 1
+    for i in range(len(inverted_list) - 1, -1, -1):
+        if inverted_list[i] == '1' and carry == 1:
+            inverted_list[i] = '0'
+        elif inverted_list[i] == '0' and carry == 1:
+            inverted_list[i] = '1'
+            carry = 0
 
-for k in range(0, lenght):
-    twos_complement += str(array[k])
-
-print(twos_complement) # Print the two's complement of the binary number
+    twos_complement = ''.join(inverted_list)
+    print(twos_complement)
